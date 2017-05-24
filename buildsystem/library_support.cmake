a4023758f0394ceeec9e3d86bc4ca65bfa079cf0
zdanowicz <zdanowiczb@gmail.com>
2017-05-24 18:57:02 +0200

    Tesseract as External Project when Tesseract_DIR not found

--- buildsystem/library_support.cmake
+++ buildsystem/library_support.cmake
@@ -27,6 +27,7 @@ FUNCTION(ADD_DEPENDENCY_TO_BOOST VERSION)
 	LINK_DIRECTORIES(${Boost_LIBRARY_DIRS})
 ENDFUNCTION(ADD_DEPENDENCY_TO_BOOST VERSION LIBRARIES)
 
+
 FUNCTION(ADD_DEPENDENCY_TO_OPEN_CV)
     FIND_PACKAGE(OpenCV REQUIRED)
     IF(NOT ${OpenCV_FOUND})
@@ -49,6 +50,7 @@ ENDFUNCTION(ADD_DEPENDENCY_TO_OPEN_CV)
 FUNCTION(ADD_DEPENDENCY_TO_LEPTONICA)
     FIND_PACKAGE(Leptonica QUIET)
     IF(NOT ${Leptonica_FOUND})
+        MESSAGE("Unable to find the requested Leptonica libraries. Adding leptonica as dependency project.")
         SET(Leptonica_Source ${CMAKE_SOURCE_DIR}/leptonica)
         include(ExternalProject)
         ExternalProject_Add(
@@ -61,15 +63,16 @@ FUNCTION(ADD_DEPENDENCY_TO_LEPTONICA)
             BINARY_DIR "${Leptonica_Source}/bin"
             SOURCE_DIR "${Leptonica_Source}"
             CMAKE_ARGS
-                -DCMAKE_INSTALL_PREFIX:PATH=${ROOT_DIR}/leptonica/install
+                -DCMAKE_INSTALL_PREFIX:PATH=${Leptonica_Source}/install
+                -DSTATIC=TRUE
         )
     
         add_library(libleptonica SHARED IMPORTED)
         
         set_property(TARGET libleptonica APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
         set_target_properties(libleptonica PROPERTIES
-            IMPORTED_IMPLIB_DEBUG "D:/Programowanie/Projects/C++/visiontext/leptonica/bin/src/Debug/leptonica-1.74.2d.lib"
-            IMPORTED_LOCATION_DEBUG "D:/Programowanie/Projects/C++/visiontext/leptonica/bin/bin/Debug/leptonica-1.74.2d.dll"
+            IMPORTED_IMPLIB_DEBUG "${Leptonica_Source}/bin/src/Debug/leptonica-1.74.2d.lib"
+            IMPORTED_LOCATION_DEBUG "${Leptonica_Source}bin/Debug/leptonica-1.74.2d.dll"
         )
         
         set_property(TARGET libleptonica APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -78,25 +81,75 @@ FUNCTION(ADD_DEPENDENCY_TO_LEPTONICA)
             IMPORTED_LOCATION_RELEASE "${Leptonica_Source}/bin/bin/Release/leptonica-1.74.2.dll"
         )
         
+        SET(EXTERNAL_PROJECTS ${EXTERNAL_PROJECTS};leptonica PARENT_SCOPE)
         SET(Leptonica_LIBS libleptonica PARENT_SCOPE)
     
     ELSE(${Leptonica_FOUND})
         SET(Leptonica_LIBS leptonica PARENT_SCOPE)
+        
     ENDIF(NOT ${Leptonica_FOUND})
-
+    
 ENDFUNCTION(ADD_DEPENDENCY_TO_LEPTONICA)
 
+
 FUNCTION(ADD_DEPENDENCY_TO_TESSERACT)
-    FIND_PACKAGE(Tesseract REQUIRED)
+    FIND_PACKAGE(Tesseract QUIET)
     IF(NOT ${Tesseract_FOUND})
-        MESSAGE(FATAL_ERROR "Unable to find the requested Tesseract libraries.")
+        MESSAGE("Unable to find the requested Tesseract libraries. Adding tesseract as dependency project.")
+        SET(Tesseract_Source ${CMAKE_SOURCE_DIR}/tesseract)
+        include(ExternalProject)
+        ExternalProject_Add(
+            tesseract
+            GIT_REPOSITORY  "https://github.com/tesseract-ocr/tesseract.git"
+            GIT_TAG "master"
+            UPDATE_COMMAND ""
+            PATCH_COMMAND ""
+            TEST_COMMAND ""
+            BUILD_COMMAND "${CMAKE_COMMAND}" --build . --target libtesseract
+            INSTALL_COMMAND ""
+            BINARY_DIR "${Tesseract_Source}/bin"
+            SOURCE_DIR "${Tesseract_Source}"
+            CMAKE_ARGS
+                -DCMAKE_INSTALL_PREFIX:PATH=${ROOT_DIR}/tesseract/install
+                -DLeptonica_DIR=${CMAKE_SOURCE_DIR}/leptonica/install/cmake
+                -DBUILD_TRAINING_TOOLS=FALSE
+                -DSTATIC=TRUE
+        )
+        
+        add_library(libtesseract SHARED IMPORTED)
+        
+        set_property(TARGET libtesseract APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+        set_target_properties(libtesseract PROPERTIES
+            IMPORTED_IMPLIB_DEBUG "${Tesseract_Source}/bin/Debug/tesseract400d.lib"
+            IMPORTED_LOCATION_DEBUG "${Tesseract_Source}/bin/bin/Debug/tesseract400d.dll"
+        )
+        
+        set_property(TARGET libtesseract APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+        set_target_properties(libtesseract PROPERTIES
+            IMPORTED_IMPLIB_RELEASE "${Tesseract_Source}/bin/Release/tesseract400.lib"
+            IMPORTED_LOCATION_RELEASE "${Tesseract_Source}/bin/bin/Release/tesseract400.dll"
+        )
+        
+        SET(EXTERNAL_PROJECTS ${EXTERNAL_PROJECTS};tesseract PARENT_SCOPE)
+        SET(Tesseract_LIBS libtesseract Ws2_32.lib PARENT_SCOPE)
+        SET(Tesseract_INCLUDE_DIRS 
+            ${Tesseract_Source}/api
+            ${Tesseract_Source}/arch
+            ${Tesseract_Source}/ccmain
+            ${Tesseract_Source}/ccstruct
+            ${Tesseract_Source}/ccutil
+            ${Tesseract_Source}/lstm
+            PARENT_SCOPE)
+              
+    ELSE(${Tesseract_FOUND})
+        MESSAGE(STATUS "Using Tesseract version: ${Tesseract_VERSION}")
+        MESSAGE(STATUS "Using Tesseract libraries from: ${Tesseract_LIBRARIES}")
+        MESSAGE(STATUS "Using Tesseract headers from: ${Tesseract_INCLUDE_DIRS}")
+        
+        SET(Tesseract_LIBS libtesseract PARENT_SCOPE)
+        SET(Tesseract_INCLUDE_DIRS ${Tesseract_INCLUDE_DIRS} PARENT_SCOPE)
     ENDIF(NOT ${Tesseract_FOUND})
 
-    MESSAGE(STATUS "Using Tesseract version: ${Tesseract_VERSION}")
-    MESSAGE(STATUS "Using Tesseract libraries from: ${Tesseract_LIBRARIES}")
-    MESSAGE(STATUS "Using Tesseract headers from: ${Tesseract_INCLUDE_DIRS}")
-    
-	SET(Tesseract_LIBS libtesseract PARENT_SCOPE)
-	SET(Tesseract_INCLUDE_DIRS ${Tesseract_INCLUDE_DIRS} PARENT_SCOPE)
 
 ENDFUNCTION(ADD_DEPENDENCY_TO_TESSERACT)
+
